3c80101df002d3954496562fb28749fa778f76bc
Ubrillo <72024415+Ubrillo@users.noreply.github.com>
2023-03-30 23:00:04 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.metrics import dp
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+class Main(BoxLayout):
+    pass
+
+class GridLayout:
+    pass
+
+class Main(App):
+    pass
+
+Main().run()
+
+
